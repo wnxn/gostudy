9f6c13ff901395fa306f1ed092d0cadbd17150fb
wnxn <wangx201401@126.com>
2018-02-15 13:16:25 +0800

    [channel]: using select, channel, buffer and timer

--- channel/select/myselect.go
+++ channel/select/myselect.go
@@ -2,59 +2,64 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"math/rand"
+	"time"
 )
 
-func myWorker(i int, c <-chan int){
-	for n := range c{ // for loop way 1
+func myWorker(i int, c <-chan int) {
+	for n := range c { // for loop way 1
+		time.Sleep(time.Second)
 		fmt.Printf("func worker %d, value = %d\n", i, n)
-	}//end for loop
-
+	} //end for loop
 }
 
-func myCreateWorker(id int)chan<-int{
+func myCreateWorker(id int) chan<- int {
 	c := make(chan int)
 	go myWorker(id, c)
 	return c
 }
 
-
-func myGenerator() <-chan int{
+func myGenerator() <-chan int {
 	c := make(chan int)
 	value := 0
-	go func(){
-		for{
+	go func() {
+		for {
 			c <- value
 			value++
-			time.Sleep(time.Millisecond *time.Duration(rand.Intn(1500)))
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(1500)))
 		}
-
 	}()
 	return c
 }
 
 func main() {
-	var c1, c2 = myGenerator(),myGenerator()
+	var c1, c2 = myGenerator(), myGenerator()
 	var worker = myCreateWorker(0)
-
-	n:=0
-	hasValue:=false
+	var values []int
+	n := 0
+	tm:=time.After(10*time.Second)
+	tick:=time.Tick(time.Second)
 	for {
 		var activeWorker chan<- int
-		if hasValue{
+		var activeValue int
+		if len(values)>0 {
 			activeWorker = worker
+			activeValue=values[0]
 		}
-
 		select {
 		case n = <-c1:
-			hasValue=true
+			values = append(values,n)
 		case n = <-c2:
-			hasValue=true
-		case activeWorker <- n:
-			hasValue = false
+			values = append(values,n)
+		case activeWorker <- activeValue:
+			values = values[1:]
+		case <-time.After(800*time.Millisecond):
+			fmt.Println("timeout")
+		case <-tick:
+			fmt.Println("queue len = ", len(values))
+		case <-tm:
+			fmt.Println("bye")
+			return
 		}
 	}
-
-
 }
